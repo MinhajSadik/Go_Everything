4788a6a289b7447ee3cd8368d7e4df256e1512ee
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-26 15:51:47 +0600

    [add] goLang-Practice file upload first time

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	a:= 49;
+	fmt.Println(a)
+}
+
+// Data Types Of Go Language
+/* 
+// Basic
+// Aggregate
+// Reference
+// Interface
+*/
+
+/* Basic types*/
+// Number
+// String
+// Boolean
+
+// Numbers//
+// Integer
+// Floating Point
+// Complex
+
+//Intergers//
+// Signed Integer +
+// Unsigned Integer +/-
+
+
