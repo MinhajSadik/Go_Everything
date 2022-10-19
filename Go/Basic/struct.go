d59d27761f66bc29085192f555a7ffb483fa066c
MinhajSadik <MinhajoSadik@gmail.com>
2022-10-19 14:07:52 +0600

    [LEARNT] GO struct from scratch

--- Go/Basic/struct.go
+++ Go/Basic/struct.go
@@ -4,6 +4,30 @@ import (
 	"fmt"
 )
 
+type Programmers struct {
+	id    int
+	name  string
+	pType string
+	field string
+}
+
+func displayInfo(programmer Programmers) {
+	fmt.Printf("id:> %v, name:> %v, pType:> %v, field:> %v\n", programmer.id, programmer.name, programmer.pType, programmer.field)
+}
+
+func (p *Programmers) changeName(name string) {
+	p.name = name
+}
+
 func Struct() {
 	fmt.Println("Struct Learning...")
+
+	minhaj := Programmers{1, "minhaj", "backend", "cse"}
+
+	displayInfo(minhaj)
+
+	minhaj.changeName("rahman")
+
+	fmt.Println(minhaj.name)
+
 }
